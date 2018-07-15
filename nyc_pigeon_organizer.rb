5fe73592241ad41a8abe898a77eddfac8e759d2c
Juliana Kang <julianajlk@gmail.com>
2018-07-15 04:15:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  pigeons_hash = {}
-  data.each do |property, hash|
-    hash.each do |attribute, array|
-      array.each do |name|
-        if pigeons_hash.has_key?(name)
-          pigeons_hash[name] = {}
+  new_hash = {}
+  data.each_pair do |attribute, values|
+    values.each_pair do |color, pigeons|
+      pigeons.each do |pigeon|
+        if new_hash[pigeon].nil?
+          new_hash[pigeon] = {}
         end
+
       end
     end
-  end
 end
