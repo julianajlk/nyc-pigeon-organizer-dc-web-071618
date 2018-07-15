307993db5c95d2d083c9e6ad8294823b317581e7
Juliana Kang <julianajlk@gmail.com>
2018-07-15 03:50:23 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if pigeons_hash.has_key?(name)
+          pigeons_hash[name] = {}
+        end
+
 end
