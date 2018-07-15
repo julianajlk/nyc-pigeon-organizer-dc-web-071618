80eeeb7cff5de8e0a7fb246ed388b77aeb269a71
Juliana Kang <julianajlk@gmail.com>
2018-07-15 04:21:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each_pair do |attribute, values|
+    values.each_pair do |color, pigeons|
+      pigeons.each do |pigeon|
+        if new_hash[pigeon].nil?
+          new_hash[pigeon] = {}
+        end
+        if new_hash[pigeon][attribute]
+          new_hash[pigeon][attribute]<< color.to_s
+        else
+          new_hash[pigeon][attribute]=[]
+          new_hash[pigeon][attribute]<<color.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
